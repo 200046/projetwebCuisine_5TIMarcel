c5477de11ec3088f3aa6173440b9cb75386682ad
200046 - TASNIER MARCEL <200046@site.asty-moulin.be>
2026-04-07 12:56:57 +0200

    correction d'erreurs

--- Views/Users/inscription.php
+++ Views/Users/inscription.php
@@ -1,38 +1,38 @@
 <div class="container">
     <h1>Inscription</h1>
-    
+
     <?php if (isset($messageError)): ?>
         <div class="alert alert-danger">
             <?= $messageError ?>
         </div>
     <?php endif; ?>
-    
+
     <form method="POST" action="/inscription">
         <div class="mb-3">
-            <label for="cat_nom" class="form-label">Nom</label>
-            <input type="text" class="form-control" id="cat_nom" name="cat_nom" required>
+            <label class="form-label">Nom</label>
+            <input type="text" class="form-control" name="nom" required>
         </div>
-        
+
         <div class="mb-3">
-            <label for="precat_nom" class="form-label">Précat_nom</label>
-            <input type="text" class="form-control" id="precat_nom" name="precat_nom" required>
+            <label class="form-label">Prénom</label>
+            <input type="text" class="form-control" name="prenom" required>
         </div>
-        
+
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control" id="email" name="email" required>
         </div>
-        
+
         <div class="mb-3">
             <label for="login" class="form-label">Login</label>
             <input type="text" class="form-control" id="login" name="login" required>
         </div>
-        
+
         <div class="mb-3">
             <label for="mot_de_passe" class="form-label">Mot de passe</label>
             <input type="password" class="form-control" id="mot_de_passe" name="mot_de_passe" required>
         </div>
-        
+
         <button type="submit" name="btnEnvoi" class="btn btn-primary">S'inscrire</button>
         <a href="/connexion" class="btn btn-link">Déjà un compte ? Connectez-vous</a>
     </form>
